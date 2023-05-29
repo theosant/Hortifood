1f80aba8169f867785383c05b44db94f572557ee
SusyDutra <susycdutra@gmail.com>
2023-05-29 16:10:51 -0300

    secoes do sitee

--- app/src/App.js
+++ app/src/App.js
@@ -30,6 +30,32 @@ function App() {
 
     return (
         <div>
+            <section className='secoes-do-site'>
+                <div className="secao">
+                    <a href="#frutas">
+                        <div className="secao-icon">
+                            <img src="images/frutas-icon.png" alt="Frutas" style={{width: 100 + 'px', height: 100 + 'px',}}/>
+                        </div>
+                        <p>Frutas</p>
+                    </a>
+                </div>
+                <div className="secao">
+                    <a href="#vegetais">
+                        <div className="secao-icon">
+                            <img src="images/vegetais-icon.png" alt="Vegetais"/>
+                        </div>
+                        <p>Vegetais</p>
+                    </a>
+                </div>
+                <div className="secao">
+                    <a href="#sucos">
+                        <div className="secao-icon">
+                            <img src="images/sucos-icon.png" alt="Sucos"/>
+                        </div>
+                        <p>Sucos</p>
+                    </a>
+                </div>
+            </section>
             <div className='main'>
                 <div className='destaques'>
                     <Carousel cardInfos = {destaques} />
