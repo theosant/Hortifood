14b66d82d3a9b8bc86594806092732c8942a6efa
susy <susycdutra@gmail.com>
2023-06-01 21:50:03 -0300

    pontapé pag login

--- app/src/index.js
+++ app/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AboutUs from './aboutus';
+import Login from './login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <AboutUs />
+    {/* <AboutUs /> */}
+    <Login />
   </React.StrictMode>
 );
 
---
+++ app/src/login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './login.css'
+import Footer from './components/Footer';
+
+const Login = (props) =>{
+
+    return (
+        <div>
+            <div className="login">
+
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Login;
